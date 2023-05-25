d178bebcb47350aaf4d9699691703d943c8ec660
stubborngastropod <dnchudnjee@gmail.com>
2023-05-25 16:59:41 +0900

    111

--- final_pjt_back/settings.py
+++ final_pjt_back/settings.py
@@ -203,9 +203,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -222,4 +219,4 @@ KAKAO_JAVASCRIPT_KEY = env('KAKAO_JAVASCRIPT_KEY')
 EXCHANGE_API_KEY = env('EXCHANGE_API_KEY')
 PRODUCTS_API_KEY = env('PRODUCTS_API_KEY')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
